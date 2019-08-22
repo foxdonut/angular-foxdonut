6a577a7e7e5099b4df6b1e68b3a644e4ca05ca38
foxdonut <fdaoud@fastmail.com>
2019-08-22 11:20:55 -0400

    adding a css framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// this should only be needed for stackblitz
+import 'bootstrap';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HelloComponent } from './component/hello/hello.component';
