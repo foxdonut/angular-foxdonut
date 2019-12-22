951ec5c98327e728bf9466cc4e74f60b85a49f35
foxdonut <fdaoud@fastmail.com>
2019-12-22 10:25:34 -0500

    Working on NgRx

--- src/app/car/car/car.component.ts
+++ src/app/car/car/car.component.ts
@@ -3,10 +3,9 @@ import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { State } from 'src/app/reducers';
+import { State as CarState } from 'src/app/car/reducers';
+
 import * as fromCar from '../reducers';
-import * as fromMake from '../make/make.reducer';
-import * as fromAvailableMakes from '../available-makes/available-makes.reducer';
-import * as fromAvailableModels from '../available-models/available-models.reducer';
 import { selectCarMake } from '../make/make.actions';
 
 @Component({
@@ -15,17 +14,12 @@ import { selectCarMake } from '../make/make.actions';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
-  availableMakes: Observable<string[]>;
-  availableModels: Observable<string[]>;
-
-  selectedMake: Observable<fromMake.State>;
+  state$: Observable<CarState>;
 
   constructor(private store: Store<State>) { }
 
   ngOnInit() {
-    this.availableMakes = this.store.pipe(select(fromCar.carFeatureKey, fromAvailableMakes.availableMakesFeatureKey));
-    this.availableModels = this.store.pipe(select(fromCar.carFeatureKey, fromAvailableModels.availableModelsFeatureKey));
-    this.selectedMake = this.store.pipe(select(fromCar.carFeatureKey, fromMake.makeFeatureKey));
+    this.state$ = this.store.pipe(select(fromCar.carFeatureKey));
   }
 
   onSelectMake(make: string) {
