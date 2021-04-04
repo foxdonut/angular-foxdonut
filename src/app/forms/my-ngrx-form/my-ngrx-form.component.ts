3fe8cee38e1b716556f88316d669210c9430d544
foxdonut <fdaoud@fastmail.com>
2021-04-04 15:46:30 -0400

    ngrx-forms dynamic ui

--- src/app/forms/my-ngrx-form/my-ngrx-form.component.ts
+++ src/app/forms/my-ngrx-form/my-ngrx-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 import { FormGroupState } from 'ngrx-forms';
-import { myNgrxFormFeatureKey, MyNgrxFormState } from './my-ngrx-form.reducer';
+import { CHOICES, GENDERS, myNgrxFormFeatureKey, MyNgrxFormState } from './my-ngrx-form.reducer';
 
 @Component({
   selector: 'app-my-ngrx-form',
@@ -10,8 +10,12 @@ import { myNgrxFormFeatureKey, MyNgrxFormState } from './my-ngrx-form.reducer';
 })
 export class MyNgrxFormComponent {
   formState$: Observable<FormGroupState<MyNgrxFormState>>;
+  dynamicUi$: Observable<any>;
+  choices = CHOICES;
+  genders = GENDERS;
 
   constructor(private readonly store: Store) {
     this.formState$ = store.pipe(select(state => state[myNgrxFormFeatureKey].formState));
+    this.dynamicUi$ = store.pipe(select(state => state[myNgrxFormFeatureKey].dynamicUi));
   }
 }
--- src/app/forms/my-ngrx-form/my-ngrx-form.reducer.ts
+++ src/app/forms/my-ngrx-form/my-ngrx-form.reducer.ts
@@ -1,8 +1,8 @@
 import { createReducer } from '@ngrx/store';
 import { createFormGroupState, FormGroupState, onNgrxForms } from 'ngrx-forms';
 
-type Choice = '' | 'Internet' | 'Phone' | 'Word of mouth' | 'Other';
-type Gender = '' | 'Female' | 'Male' | 'Non-binary';
+export const CHOICES = ['Internet', 'Phone', 'Word of mouth', 'Other'];
+export const GENDERS = ['Female', 'Male', 'Non-binary'];
 
 export const myNgrxFormFeatureKey = 'myNgrxForm';
 
@@ -12,24 +12,34 @@ export interface MyNgrxFormState {
   email: string;
   username: string;
   password: string;
-  howDidYouHear: Choice;
-  gender: Gender;
+  howDidYouHear: string;
+  gender: string;
+  dynamic: Array<any>;
 }
 
 export interface ExampleNgrxFormState {
   other: string;
   formState: FormGroupState<MyNgrxFormState>;
+  dynamicUi: Array<any>;
 }
 
 const initialState: ExampleNgrxFormState = {
   other: 'example',
   formState: createFormGroupState<MyNgrxFormState>(FORM_ID, {
     email: '',
-    username: '',
+    username: 'Duck',
     password: '',
     howDidYouHear: '',
-    gender: ''
-  })
+    gender: '',
+    dynamic: [
+      false,
+      ''
+    ]
+  }),
+  dynamicUi: [
+    { id: 42, dynamicType: 'checkbox', label: 'Dynamic checkbox' },
+    { id: 24, dynamicType: 'text', label: 'Dynamic text field' }
+  ]
 };
 
 export const myNgrxFormReducer = createReducer(
